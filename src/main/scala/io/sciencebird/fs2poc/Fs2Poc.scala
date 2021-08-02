225d6b29370980aae8537aa42303f78181fdaadc
Pablo Cavallo <pac@codebox.com.ar>
2021-08-02 18:05:57 -0300

    low level api sample

--- src/main/scala/io/sciencebird/fs2poc/Fs2Poc.scala
+++ src/main/scala/io/sciencebird/fs2poc/Fs2Poc.scala
@@ -3,13 +3,15 @@ package io.sciencebird.fs2poc
 import cats.effect.ExitCode
 import cats.effect.IO
 import cats.effect.IOApp
+import cats.implicits._
 import fs2.io.file.Files
 import java.nio.file.Paths
 import fs2.text
+import fs2.data.csv.lowlevel
 
 object Fs2Poc extends IOApp {
 
-  override def run(args: List[String]): IO[ExitCode] = {
+  def countingProgram: IO[ExitCode] = {
     val s = Files[IO]
       .readAll(Paths.get("testdata/IndexData.csv"), 4096)
       .through(text.utf8Decode)
@@ -23,4 +25,21 @@ object Fs2Poc extends IOApp {
 
   }
 
+  def lowLevelApi: IO[ExitCode] = {
+
+    val s = Files[IO]
+      .readAll(Paths.get("testdata/IndexProcessed.csv"), 4096)
+      .through(text.utf8Decode)
+      .through(lowlevel.rows[IO, String]())
+      .take(10)
+      .through(lowlevel.headers[IO, String])
+
+    for {
+      _      <- IO.println("Starting")
+      result <- s.compile.toList
+      _      <- result.traverse(IO.println)
+    } yield ExitCode.Success
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = lowLevelApi
 }
